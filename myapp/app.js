416bd15c313f2667ee6dc3b4359d3c29fa504f10
PeiYumeng <457492875@qq.com>
2021-01-22 19:28:33 +0800

    message

--- myapp/app.js
+++ myapp/app.js
@@ -41,5 +41,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+app.get('/', (req, res, next) => {
+  console.log(Object.keys(req))
+  console.log(req.headers)
+  console.log(req.query)
+  console.log(Object.keys(res))
+  res.send('Hello World')
+})
 
+app.listen(port, () => {
+  console.log(`express server start at http://localhost:${port}`)
+})
 module.exports = app;
